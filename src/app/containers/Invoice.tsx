d43b2db4541f8e9436eecd0992bda3f39ba71204
Jackson Huang <jacksonh201325@gmail.com>
2019-10-13 21:47:44 -0300

    Created Incoice Container

---
+++ src/app/containers/Invoice.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Invoice extends Component {
+  render() {
+    return (
+      <div className="invoice">
+        <p>Hello</p>
+      </div>
+    );
+  }
+}
+
+export default Invoice;
