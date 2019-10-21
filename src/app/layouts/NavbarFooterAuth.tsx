6dd1cb572b60547df37d1e7634458a98b279cbb8
Jackson Huang <jacksonh201325@gmail.com>
2019-10-21 19:02:22 -0300

    Implemented material UI

--- src/app/layouts/NavbarFooterAuth.tsx
+++ src/app/layouts/NavbarFooterAuth.tsx
@@ -1,11 +1,62 @@
 import React from 'react';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import './NavbarFooterAuth.scss';
+import { Link } from 'react-router-dom';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1
+    },
+    menuButton: {
+      marginRight: theme.spacing(2)
+    },
+    title: {
+      flexGrow: 1
+    }
+  })
+);
 
 export const NavbarFooterAuth = props => {
+  const classes = useStyles();
+  const [auth, setAuth] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAuth(event.target.checked);
+  };
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
-    <div className="navbar">
-      <h1>Hello This is Navbar Auth</h1>
-      <h1>{props.children}</h1>
-      <h1>Hello this is footer Auth</h1>
+    <div className="NavbarFooterAuth">
+      <AppBar position="static" className="app-bar">
+        <Toolbar className="tool-bar">
+          <Typography className="logo" variant="h5" color="inherit">
+            Invoice Manager
+          </Typography>
+          <div>
+            <Typography variant="h6" color="inherit">
+              <Link className="link" to="/signup">
+                Register
+              </Link>
+            </Typography>
+          </div>
+        </Toolbar>
+      </AppBar>
+      {props.children}
+      <Typography variant="h2" gutterBottom>
+        Hello this is footer Auth
+      </Typography>
     </div>
   );
 };
--- src/app/views/auth-views/AuthLogin.tsx
+++ src/app/views/auth-views/AuthLogin.tsx
@@ -1,15 +1,45 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { NavbarFooterAuth } from '../../layouts/NavbarFooterAuth';
 
+import clsx from 'clsx';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import Input from '@material-ui/core/Input';
+import TextField from '@material-ui/core/TextField';
+
 export const AuthLogin = props => {
+  const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+      container: {
+        display: 'flex',
+        flexWrap: 'wrap'
+      },
+      input: {
+        margin: theme.spacing(1)
+      }
+    })
+  );
+  const classes = useStyles();
+
   return (
     <NavbarFooterAuth>
       <div className="Login">
-        {props.location.pathname}
-        <h1>Hello this is login</h1>
-        <Link to="/admin/dashboard">Dashboard</Link>
-        <Link to="/signup">Register</Link>
+        <h1>Login</h1>
+        <form className={classes.container} noValidate autoComplete="off">
+          <Input
+            placeholder="Username"
+            className={classes.input}
+            inputProps={{
+              'aria-label': 'username'
+            }}
+          />
+          <Input
+            placeholder="Password"
+            className={classes.input}
+            inputProps={{
+              'aria-label': 'password'
+            }}
+          />
+        </form>
       </div>
     </NavbarFooterAuth>
   );
