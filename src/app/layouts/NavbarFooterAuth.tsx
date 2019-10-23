29e74e8eaba2c56ad23b9aac8726c71ff9a5dae7
Jackson Huang <jacksonh201325@gmail.com>
2019-10-23 04:49:53 -0300

    Styled login page

--- src/app/layouts/NavbarFooterAuth.tsx
+++ src/app/layouts/NavbarFooterAuth.tsx
@@ -3,8 +3,10 @@ import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import PersonAddIcon from '@material-ui/icons/PersonAdd';
+
 import './NavbarFooterAuth.scss';
-import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -16,27 +18,23 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     title: {
       flexGrow: 1
+    },
+    button: {
+      background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+      border: 0,
+      borderRadius: 3,
+      color: 'white',
+      height: 36,
+      padding: '0 12px'
+    },
+    input: {
+      display: 'none'
     }
   })
 );
 
 export const NavbarFooterAuth = props => {
   const classes = useStyles();
-  const [auth, setAuth] = React.useState(true);
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setAuth(event.target.checked);
-  };
-
-  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
   return (
     <div className="NavbarFooterAuth">
       <AppBar position="static" className="app-bar">
@@ -46,17 +44,20 @@ export const NavbarFooterAuth = props => {
           </Typography>
           <div>
             <Typography variant="h6" color="inherit">
-              <Link className="link" to="/signup">
+              <Button
+                variant="outlined"
+                color="secondary"
+                size="medium"
+                className={classes.button}
+                startIcon={<PersonAddIcon />}
+              >
                 Register
-              </Link>
+              </Button>
             </Typography>
           </div>
         </Toolbar>
       </AppBar>
       {props.children}
-      <Typography variant="h2" gutterBottom>
-        Hello this is footer Auth
-      </Typography>
     </div>
   );
 };
--- src/app/views/auth-views/AuthLogin.tsx
+++ src/app/views/auth-views/AuthLogin.tsx
@@ -1,20 +1,66 @@
 import React from 'react';
 import { NavbarFooterAuth } from '../../layouts/NavbarFooterAuth';
-
-import clsx from 'clsx';
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
-import Input from '@material-ui/core/Input';
-import TextField from '@material-ui/core/TextField';
+import './AuthLogin.scss';
+import {
+  Typography,
+  Card,
+  Grid,
+  CardContent,
+  Button,
+  FormControl,
+  TextField,
+  OutlinedInput,
+  InputLabel,
+  Input,
+  makeStyles,
+  Theme,
+  createStyles,
+  IconButton
+} from '@material-ui/core';
 
 export const AuthLogin = props => {
   const useStyles = makeStyles((theme: Theme) =>
     createStyles({
-      container: {
-        display: 'flex',
-        flexWrap: 'wrap'
+      root: {
+        flexGrow: 1
+      },
+      textField: {
+        marginLeft: theme.spacing(1),
+        marginRight: theme.spacing(1),
+        flexGrow: 1
       },
       input: {
         margin: theme.spacing(1)
+      },
+      card: {
+        marginTop: theme.spacing(6),
+        height: 'auto',
+        width: 400,
+        margin: 'auto'
+      },
+      header: {
+        textAlign: 'center',
+        padding: theme.spacing(4)
+      },
+      formControl: {
+        margin: theme.spacing(1)
+      },
+      form: {
+        display: 'flex',
+        width: 320,
+        margin: 'auto',
+        justifyContent: 'center'
+      },
+      button: {
+        background: 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)',
+        borderRadius: 3,
+        border: 0,
+        color: 'white',
+        height: 48,
+        boxShadow: '0 3px 5px 2px rgba(33, 203, 243, .3)',
+        marginTop: 24,
+        width: 200,
+        margin: 'auto'
       }
     })
   );
@@ -22,25 +68,67 @@ export const AuthLogin = props => {
 
   return (
     <NavbarFooterAuth>
-      <div className="Login">
-        <h1>Login</h1>
-        <form className={classes.container} noValidate autoComplete="off">
-          <Input
-            placeholder="Username"
-            className={classes.input}
-            inputProps={{
-              'aria-label': 'username'
-            }}
-          />
-          <Input
-            placeholder="Password"
-            className={classes.input}
-            inputProps={{
-              'aria-label': 'password'
-            }}
-          />
-        </form>
-      </div>
+      <Grid container justify="center" className={classes.root}>
+        <Grid item md={6}>
+          <Card className={classes.card}>
+            <div className={classes.header}>
+              <Typography variant="h2">Login</Typography>
+            </div>
+            <CardContent>
+              {/* <FormControl className={classes.form}>
+                <TextField
+                  id="standard-password-input"
+                  label="Username"
+                  className={classes.textField}
+                  type="text"
+                  autoComplete="current-password"
+                  margin="normal"
+                />
+                <TextField
+                  id="standard-password-input"
+                  label="Password"
+                  className={classes.textField}
+                  type="password"
+                  autoComplete="current-password"
+                  margin="normal"
+                />
+                <Button
+                  variant="outlined"
+                  color="secondary"
+                  size="medium"
+                  className={classes.button}
+                >
+                  Login
+                </Button>
+              </FormControl> */}
+              <Formik
+                initialValues={{ name: 'jared' }}
+                onSubmit={(values, actions) => {
+                  setTimeout(() => {
+                    alert(JSON.stringify(values, null, 2));
+                    actions.setSubmitting(false);
+                  }, 1000);
+                }}
+                render={props => (
+                  <form onSubmit={props.handleSubmit}>
+                    <input
+                      type="text"
+                      onChange={props.handleChange}
+                      onBlur={props.handleBlur}
+                      value={props.values.name}
+                      name="name"
+                    />
+                    {props.errors.name && (
+                      <div id="feedback">{props.errors.name}</div>
+                    )}
+                    <button type="submit">Submit</button>
+                  </form>
+                )}
+              />
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
     </NavbarFooterAuth>
   );
 };
