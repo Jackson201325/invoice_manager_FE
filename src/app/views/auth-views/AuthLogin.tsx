cdab371221b287ddf976aa2d03ab6dca598cb994
Jackson Huang <jacksonh201325@gmail.com>
2019-10-27 00:13:32 -0300

    Created Login API

--- src/app/views/auth-views/AuthLogin.tsx
+++ src/app/views/auth-views/AuthLogin.tsx
@@ -17,51 +17,69 @@ import { withRouter } from 'react-router-dom';
 
 import './AuthLogin.scss';
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1
+    },
+    textField: {
+      marginBottom: theme.spacing(3)
+    },
+    input: {
+      margin: theme.spacing(1)
+    },
+    card: {
+      marginTop: theme.spacing(6),
+      height: 'auto',
+      width: 400,
+      margin: 'auto'
+    },
+    header: {
+      textAlign: 'center',
+      padding: theme.spacing(4)
+    },
+    formControl: {
+      margin: theme.spacing(1)
+    },
+    form: {
+      display: 'flex',
+      width: 320,
+      margin: 'auto',
+      flexDirection: 'column'
+    },
+    button: {
+      background: 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)',
+      borderRadius: 3,
+      border: 0,
+      color: 'white',
+      height: 48,
+      boxShadow: '0 3px 5px 2px rgba(33, 203, 243, .3)',
+      marginTop: 24,
+      width: 200,
+      margin: 'auto'
+    }
+  })
+);
+
 const AuthLogin = () => {
-  const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-      root: {
-        flexGrow: 1
-      },
-      textField: {
-        marginBottom: theme.spacing(3)
-      },
-      input: {
-        margin: theme.spacing(1)
-      },
-      card: {
-        marginTop: theme.spacing(6),
-        height: 'auto',
-        width: 400,
-        margin: 'auto'
-      },
-      header: {
-        textAlign: 'center',
-        padding: theme.spacing(4)
-      },
-      formControl: {
-        margin: theme.spacing(1)
-      },
-      form: {
-        display: 'flex',
-        width: 320,
-        margin: 'auto',
-        flexDirection: 'column'
-      },
-      button: {
-        background: 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)',
-        borderRadius: 3,
-        border: 0,
-        color: 'white',
-        height: 48,
-        boxShadow: '0 3px 5px 2px rgba(33, 203, 243, .3)',
-        marginTop: 24,
-        width: 200,
-        margin: 'auto'
-      }
-    })
-  );
   const classes = useStyles();
+
+  const handleSubmit = (values, { props, setSubmitting }) => {
+    axios
+      .post(`http://localhost:3000/auth/login`, values)
+      .then(response => {
+        localStorage.setItem('auth_token', response.data.auth_token);
+        console.log(props);
+      })
+      .catch(err => {
+        alert(err);
+      });
+    // actions.setSubmitting(false);
+    //process form submission here
+    //done submitting, set submitting to false
+    setSubmitting(false);
+    return;
+  };
 
   return (
     <Grid container justify="center" className={classes.root}>
