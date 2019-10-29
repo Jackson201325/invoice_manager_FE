e95e273363d7b808ec52b39378394503e1544c7c
Jackson Huang <jacksonh201325@gmail.com>
2019-10-29 13:50:23 -0300

    Factorization

--- src/app/views/auth-views/LoginForm.tsx
+++ src/app/views/auth-views/LoginForm.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { authStyles } from '../../styles/authStyle';
 import {
   Typography,
   Card,
@@ -7,57 +8,11 @@ import {
   Button,
   InputLabel,
   Input,
-  makeStyles,
-  Theme,
-  createStyles
 } from '@material-ui/core';
 import { Formik } from 'formik';
 
 export const LoginForm = props => {
-  const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-      root: {
-        flexGrow: 1
-      },
-      textField: {
-        marginBottom: theme.spacing(3)
-      },
-      input: {
-        margin: theme.spacing(1)
-      },
-      card: {
-        marginTop: theme.spacing(6),
-        height: 'auto',
-        width: 400,
-        margin: 'auto'
-      },
-      header: {
-        textAlign: 'center',
-        padding: theme.spacing(4)
-      },
-      formControl: {
-        margin: theme.spacing(1)
-      },
-      form: {
-        display: 'flex',
-        width: 320,
-        margin: 'auto',
-        flexDirection: 'column'
-      },
-      button: {
-        background: 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)',
-        borderRadius: 3,
-        border: 0,
-        color: 'white',
-        height: 48,
-        boxShadow: '0 3px 5px 2px rgba(33, 203, 243, .3)',
-        marginTop: 24,
-        width: 200,
-        margin: 'auto'
-      }
-    })
-  );
-  const classes = useStyles();
+  const classes = authStyles();
   return (
     <Grid container justify="center" className={classes.root}>
       <Grid item md={6}>
@@ -70,7 +25,7 @@ export const LoginForm = props => {
               initialValues={{ email: '', password: '' }}
               onSubmit={(values, actions) => {
                 actions.setSubmitting(true);
-                props.login(values).then(() => console.log('ok'));
+                props.login(values).then(() => actions.setSubmitting(false));
               }}
               render={props => (
                 <form className={classes.form} onSubmit={props.handleSubmit}>
